16bcbfc3a584ace4588407734ebdc7144170a442
Artem Sokovets <relix@mail.ru>
2017-11-19 01:07:13 +0300

    add missing import classes

--- src/test/java/io/github/artsok/travis/steps/StepDefinitions.java
+++ src/test/java/io/github/artsok/travis/steps/StepDefinitions.java
@@ -2,6 +2,10 @@ package io.github.artsok.travis.steps;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import io.github.artsok.travis.pages.FilterPage;
 import io.github.artsok.travis.pages.LandingPage;
 import io.github.artsok.travis.pages.ProductPage;
